5be1e728c3e62b9353ad22a35c7503407a85bd76
Mateus Yukio <mateusyukio10@hotmail.com>
2023-09-11 23:38:18 -0300

    Queen

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -11,6 +11,7 @@ import chess.piece.Bishop;
 import chess.piece.King;
 import chess.piece.Knight;
 import chess.piece.Pawn;
+import chess.piece.Queen;
 import chess.piece.Rook;
 
 public class ChessMatch {
@@ -204,6 +205,7 @@ public class ChessMatch {
 	
 	
 	private void initialSetup() {
+		placeNewPiece('d', 1, new Queen(board, Color.WHITE));
 		placeNewPiece('b', 1, new Knight(board, Color.WHITE));
 		placeNewPiece('g', 1, new Knight(board, Color.WHITE));
 		placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
@@ -220,6 +222,7 @@ public class ChessMatch {
         placeNewPiece('g', 2, new Pawn(board, Color.WHITE));
         placeNewPiece('h', 2, new Pawn(board, Color.WHITE));
 
+		placeNewPiece('d', 8, new Queen(board, Color.BLACK));
 		placeNewPiece('b', 8, new Knight(board, Color.BLACK));
 		placeNewPiece('g', 8, new Knight(board, Color.BLACK));
 		placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
